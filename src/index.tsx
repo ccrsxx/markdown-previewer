e6760eaa27fed5728dab2e1ef925d325a49ab28b
ccrsxx <55032197+ccrsxx@users.noreply.github.com>
2022-02-17 19:04:57 +0700

    add strict mode

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './styles/index.scss';
@@ -5,4 +6,8 @@ import './styles/index.scss';
 const container = document.getElementById('root') as HTMLDivElement;
 const root = ReactDOM.createRoot(container);
 
-root.render(<App />);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
